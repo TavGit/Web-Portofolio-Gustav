5d19ee3ea2b816fadc5ac56c6a8f6a8fa72bc63e
TavGit <gustavbaim18@gmail.com>
2025-05-06 21:55:17 +0700

    melakukan perubahan di vite js dan package js

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Web-Portofolio-Gustav/',
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  },
-  json: {
-    stringify: true,
-  },
-});
+})
